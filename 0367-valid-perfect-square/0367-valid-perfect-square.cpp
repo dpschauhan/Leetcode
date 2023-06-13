63eea4fecda53c7b3a76d286d29e4a6774d465a0
Divyansh <93876710+dpschauhan@users.noreply.github.com>
2023-06-13 11:51:55 +0530

    Time: 3 ms (43.54%), Space: 6.1 MB (8.99%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
+        // Square numbers end with 0, 1, 4, 5, 6 or 9
+        int ld = num % 10; //last digit
+        if(ld==2 ||ld==3|| ld==7 || ld==8 ){
+            return 0;
+        }
+        else {
+            long long int i=0;
+            while (i*i<=num){
+                if(i*i==num){
+                    return 1; 
+                }
+                i++;
+            }
+            return 0;
+        }
+    }
+};
+class Solution1 {
+public:
+    bool isPerfectSquare(int num) {
         long long int i=0;
         while (i*i<=num){
             if(i*i==num){
