03258684339de76935bf8ba9f18d1c623b79eaf3
Divyansh <93876710+dpschauhan@users.noreply.github.com>
2023-06-16 18:39:06 +0530

    Time: 1696 ms (5.01%), Space: 269.6 MB (5.42%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int countArrangement(int n) {
         vector<bool> bitvec(n+1,true);
@@ -10,6 +10,31 @@ public:
 private:
     void s(int n,vector<bool> bitvec, vector<int> p,vector<vector<int>>& all){
         if(p.size()==n )all.push_back(p);
+        else{
+            for(int i=1;i<=n;i++){
+                if(bitvec[i] && ((i%(p.size()+1)==0 || ((p.size()+1) %i==0)))){
+                    bitvec[i]=0;
+                    p.push_back(i);
+                    s(n,bitvec,p,all);
+                    p.pop_back();
+                    bitvec[i]=1;
+                    }
+            }
+        }
+    }
+};
+class Solution {
+public:
+    int countArrangement(int n) {
+        vector<bool> bitvec(n+1,true);
+        vector<int> p;
+        int all=0;
+        s(n,bitvec,p,all);
+        return all;
+    }
+private:
+    void s(int n,vector<bool> bitvec, vector<int> p,int& all){
+        if(p.size()==n )all++;
         else{
             for(int i=1;i<=n;i++){
                 if(bitvec[i] && ((i%(p.size()+1)==0 || ((p.size()+1) %i==0)))){
