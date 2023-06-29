1e7e11c389b48ab60524de4d47e3a34498ea96ac
Divyansh <93876710+dpschauhan@users.noreply.github.com>
2023-06-29 19:11:47 +0530

    Time: 0 ms (100%), Space: 7.8 MB (46.75%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,25 @@
+class Solution1 {
+public:
+    int coinRow(vector<int>& a,int n){
+        if(n==0)return 0;
+        if(n==1)return a[0];
+        int n1=coinRow(a,n-1);
+        int n2=a[n-1]+coinRow(a,n-2);
+        return max(n1,n2);
+    }
+    int rob(vector<int>& nums) {
+        return coinRow(nums,nums.size());
+    }
+};
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> tbl(nums.size()+1);
+        tbl[0]=0 , tbl[1]=nums[0];
+        for (int n=2;n<=nums.size();n++){
+            tbl[n]=max(tbl[n-1],nums[n-1]+ tbl[n-2]);
+        }
+        return tbl[nums.size()];
+        
+    }
+};
