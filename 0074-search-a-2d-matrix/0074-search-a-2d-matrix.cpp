a5ee084a0a582aab7fac85c61fcb47c4991e9730
Divyansh <93876710+dpschauhan@users.noreply.github.com>
2023-09-07 06:32:36 -0700

    Time: 4 ms (32.93%), Space: 9.5 MB (29.3%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for (int i=0;i<n;i++){
+            if(matrix[i][m-1] == target){
+                return 1;
+            }
+            if(matrix[i][m-1] > target){
+                for (int j= 0;j<m;j++){
+                    if(matrix[i][j] == target){
+                        return 1;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
+class Solution1 {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
         for (int i=0;i< matrix.size();i++){
             for (int j=0;j<matrix[0].size();j++){
                 if (matrix[i][j]==target){
